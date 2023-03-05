a7be60ea6dca79c9e2950094cb3c5611a1eb2de1
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2023-03-06 00:47:41 +0100

    add middleware for user

--- src/middlewares/UserMiddemware.ts
+++ src/middlewares/UserMiddemware.ts
@@ -1,5 +1,32 @@
-/*import { NextFunction, Request, Response } from "express"
-
+import { NextFunction, Request, Response } from "express"
+import Connexion from "../controllers/ConnexionController"
 import  {RoleUser}  from "../models/IUser"
 
-*/
+
+
+export const isAdmin =(req:Request,res:Response,next: NextFunction)=>{
+
+    if(Connexion.userConnected.role===RoleUser.Admin){
+        next()
+    }else{
+        res.send({message:"you are not an admin"})
+    }
+}
+
+export const isArtiste =(req:Request,res:Response,next: NextFunction)=>{
+    
+    if(Connexion.userConnected.role===RoleUser.Artiste){
+        next()
+    }else{
+        res.send({message:"you are not an artiste"})
+    }
+}
+
+export const isManager =(req:Request,res:Response,next: NextFunction)=>{
+    
+    if(Connexion.userConnected.role===RoleUser.Manager){
+        next()
+    }else{
+        res.send({message:"you are not a manager"})
+    }
+}
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express"
 import User from "../controllers/UserController"
+import {isAdmin} from "../middlewares/UserMiddemware"
 const userRouter = express.Router()
 
 
 userRouter.get("/users",  User.getAllUser)
 userRouter.post("/register",   User.createUser)
-userRouter.delete("/user/:id",User.deleteUser)
+userRouter.delete("/user/:id",isAdmin, User.deleteUser)
 userRouter.patch("/user/:id",User.updateUser)
 
 export default userRouter
